9bb39cb7dafa7c9fac2b07d4f7fd517ae4663d6f
K3be <ahmed.13.alkaaby@gmail.com>
2019-12-07 17:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ function findMatching(arr,string){
     return arr;
 }
 
-function fuzzyMatch(arr,string){
-    arr.filter(function(f){
-        return f.indexof(string)===0;
-    })
-    return arr;
+function fuzzyMatch(arr,s){
+   arr= arr.filter(function(v){
+    return v.toLowerCase().indexOf(s.toLowerCase()) === 0;
+  })
+  return arr;
 }
+
 
 function matchName(arr,string){
     arr=arr.filter(function(f){
